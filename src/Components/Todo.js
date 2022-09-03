662d7c12dbec059917651057bdf78bc4c2201548
vishalparjapat10 <vishalparjapat12527@gmail.com>
2022-09-03 15:05:12 +0530

    :art: UI completed

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -4,12 +4,38 @@ import './Todo.css'
 export default class Todo extends Component {
     constructor(){
         super();
+        this.state = {
+            tasks:[{task:"JavaScript"}]
+        }
     }
   render() {
     return (
-      <div className='header'>
-        <h1>Todo App</h1>
-      </div>
+        <div className='todo-cont'>
+            <div className='todo-navbar'>
+                <h1>Todo App</h1>
+            </div>
+            <div className='todo-input'>
+                <input type="text" onChange={this.handleChange}></input>
+                <button>Add</button>
+            </div>
+            <div className='todo-tasks'>
+                {
+                    this.state.tasks.length == 0 ?
+                    <h1>
+                        List is Empty!<br></br>Please Add Tasks
+                    </h1>
+                    :
+                    this.state.tasks.map((taskObj) => (
+                        <li className='todo-task'>
+                            <h2>{taskObj.task}</h2>
+                            <button>Delete</button>
+                        </li>
+                    ))
+                    
+                }
+            </div>
+        </div>
+      
     )
   }
 }
