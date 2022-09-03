71f792797327a3ae545bad48cc288c91c59ec681
vishalparjapat10 <vishalparjapat12527@gmail.com>
2022-09-03 14:34:51 +0530

    :tada: app started

---
+++ src/Components/Todo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Todo.css'
+
+export default class Todo extends Component {
+    constructor(){
+        super();
+    }
+  render() {
+    return (
+      <div className='header'>
+        <h1>Todo App</h1>
+      </div>
+    )
+  }
+}
